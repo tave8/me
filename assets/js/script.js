df3801797dacbf8bac2bfafcb3900fb6879dda6f
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-25 06:57:24 +0100

    add typewrite

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", () => {
+  
+  const typewriterMyDescription = new TypeWriter({
+    elementSelector: "main > .title > .my-description",
+    text: "i love solving problems and writing code that humans love reading_",
+    speed: "fast",
+    onFinishTypewrite: () => {},
+  });
+
+  const typewriterMyTitle = new TypeWriter({
+    elementSelector: "main > .title > .my-title",
+    text: "{ software developer }",
+    onFinishTypewrite: () => {
+        typewriterMyDescription.run()
+    },
+  });
+
+  const typewriterMyName = new TypeWriter({
+    elementSelector: "main > .title > .my-name",
+    text: "Giuseppe Tavella_",
+    speed: "slow",
+    onFinishTypewrite: () => {
+      typewriterMyTitle.run();
+    },
+  });
+
+  typewriterMyName.run();
+});
