57fbcad8d979ce41b511e914ef6809fd8e03828d
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-25 07:39:35 +0100

    less code typewrite feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,21 @@
-window.addEventListener("load", () => {
-  
-  const typewriterMyDescription = new TypeWriter({
-    elementSelector: "main > .title > .my-description",
-    text: "i love solving problems and writing code that humans love reading_",
-    speed: "fast",
-    onFinishTypewrite: () => {},
-  });
-
-  const typewriterMyTitle = new TypeWriter({
-    elementSelector: "main > .title > .my-title",
-    text: "{ software developer }",
-    onFinishTypewrite: () => {
-        typewriterMyDescription.run()
-    },
-  });
-
-  const typewriterMyName = new TypeWriter({
-    elementSelector: "main > .title > .my-name",
-    text: "Giuseppe Tavella_",
-    speed: "slow",
-    onFinishTypewrite: () => {
-      typewriterMyTitle.run();
-    },
-  });
-
-  typewriterMyName.run();
+const typewriterMyDescription = new TypeWriter({
+  elementSelector: "main > .title > .my-description",
+  text: "i love solving problems and writing code that humans love reading_",
+  speed: "fast",
+  onFinishTypewrite: () => {},
 });
+
+const typewriterMyTitle = new TypeWriter({
+  elementSelector: "main > .title > .my-title",
+  text: "{ software developer }",
+  onFinishTypewrite: () => {},
+});
+
+const typewriterMyName = new TypeWriter({
+  elementSelector: "main > .title > .my-name",
+  text: "Giuseppe Tavella_",
+  speed: "slow",
+  onFinishTypewrite: () => {},
+});
+
+TypeWriter.runOneAfterAnother([typewriterMyName, typewriterMyTitle, typewriterMyDescription]);
